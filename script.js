070c1a5e097d7399aba17999df79a72576512a89
kristen <pumikinforhire@gmail.com>
2020-08-27 20:33:29 -0500

    Added reset button functionality

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector('#grid-container');
 
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', getNum);
+
+function getNum() {
+    let num = prompt("How many squares would you like your grid to be?");
+    createGrid(num);
+}
+
 function createGrid(numDivs) {
     container.style.gridTemplateColumns = `repeat(${numDivs}, 1fr)`;
     container.style.gridTemplateRows = `repeat(${numDivs}, 1fr)`;
@@ -7,5 +15,10 @@ function createGrid(numDivs) {
         const div = document.createElement('div');
         div.classList.add('grid-div');
         container.appendChild(div);
+        div.addEventListener('mouseover', changeColor);
     }
+}
+
+function changeColor() {
+    this.style.backgroundColor = 'black';
 }
